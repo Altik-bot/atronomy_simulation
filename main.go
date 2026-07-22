7841d3f5d2892d404d5e8ff77c1d1d43007b0fdf
burdebar <nurbek.aibar0110@gmail.com>
2026-07-22 10:12:26 +0500

    Refactor main function and inline helper functions

--- main.go
+++ main.go
@@ -5,20 +5,8 @@ import (
 	"math"
 )
 
-func form(t1 int, t2 int, r int) float64 {
-	const G int = 2
-	var F float64 = float64(G * ((t1 * t2) / (r * r)))
-	fmt.Println(F)
-	return F
-}
-
-func dist(x1, y1, x2, y2 float64) float64 {
-	var distance float64 = math.Sqrt((math.Pow(x2-x1, 2)) + (math.Pow(y2-y1, 2)))
-	fmt.Println(distance)
-	return distance
-}
-
 func main() {
+	const g int = 2
 	var m1 int = 10
 	var m2 int = 20
 	var radius int = 2
@@ -26,6 +14,16 @@ func main() {
 	var m1_y float64 = 1
 	var m2_x float64 = 7
 	var m2_y float64 = 6
-	form(m1, m2, radius)
-	dist(m1_x, m1_y, m2_x, m2_y)
+	form := func() float64 {
+		var F float64 = float64(g * ((m1 * m2) / (radius * radius)))
+		fmt.Println(F)
+		return F
+	}
+	dist := func() float64 {
+		var distance float64 = math.Sqrt((math.Pow(m2_x-m1_x, 2)) + (math.Pow(m2_y-m1_y, 2)))
+		fmt.Println(distance)
+		return distance
+	}
+	form()
+	dist()
 }
